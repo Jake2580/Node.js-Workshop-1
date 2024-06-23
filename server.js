8c5560afeaab34196a19e8ad8d88410449d27f33
Jake <86923926+Jake2580@users.noreply.github.com>
2024-06-23 20:06:54 +0900

    “Session” has been cleaned up.

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ APP.use(express.static('public'));
 
 ////// Database
 const mongoclient = require('mongodb').MongoClient;
-const ObjId = require('mongodb').ObjectId;
 const DB_URI = dotenv.parsed.DB_URI;
 let mydb;
 
@@ -31,53 +30,13 @@ mongoclient.connect(DB_URI).then(client => {
 });
 ////////////////////
 
-////// Session
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+////// Session 및 Passport 설정
+const session = require('express-session');
+const routes_session = require('./routes/session');
 
-let session = require('express-session');
-const MongoStore = require('connect-mongo');
-const sha = require('sha256');
-
-APP.use(session({
-    secret: 'dkufe8938493j4e08349u',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-        mongoUrl: DB_URI
-    })
-}));
-APP.use(passport.initialize());
-APP.use(passport.session());
-
-passport.serializeUser(function (user, done) {
-    done(null, user.userid);
-});
-
-passport.deserializeUser(function (puserid, done) {
-    mydb.collection('account').findOne({ userid: puserid }).then((result) => {
-        done(null, result);
-    });
-});
-
-passport.use(new LocalStrategy({
-    usernameField: "userid",
-    passwordField: "userpw",
-    session: true,
-    passReqToCallback: false,
-}, function (inputid, inputpw, done) {
-    mydb.collection('account').findOne({ userid: inputid }).then((result) => {
-        if (result == null) {
-            return done(null, false, { message: "아이디가 존재하지 않습니다" });;
-        }
-
-        if (result.userpw == sha(inputpw)) {
-            return done(null, result); // 새로운 로그인
-        }
-
-        return done(null, false, { message: "비밀번호 틀렸어요" });
-    });
-}));
+APP.use(session(routes_session.sessionConfig));
+APP.use(routes_session.passport.initialize());
+APP.use(routes_session.passport.session());
 ////////////////////
 
 ////// Routes
@@ -86,7 +45,7 @@ APP.use('/', require('./routes/budongsan.js'));
 APP.use('/', require('./routes/auth.js'));
 ////////////////////
 
-////// server route
+////// Home
 APP.get('/', function (req, res) {
     let user;
     if (req.session.passport) { user = req.session.passport; } else { user = req.user; }
