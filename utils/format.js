9c90a0d5b01cd3696213d5409e2a05db57128e64
Jake <86923926+Jake2580@users.noreply.github.com>
2024-06-25 19:19:14 +0900

    Change variable name.

--- utils/format.js
+++ utils/format.js
@@ -2,11 +2,10 @@ function formatNumber(number) {
     return `${number}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function getCurrentDateString() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
+function getCurrentDateString(date = new Date()) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 }
 
